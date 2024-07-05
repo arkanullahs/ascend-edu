ad5c2807692c4d8b99194f0998b4f8b41141049e
arkanullahs <arkanullah.cse.20220104011@aust.edu>
2024-07-06 00:08:13 +0600

    added logout buggy

--- frontend/src/components/layout/navigation/Navigation.js
+++ frontend/src/components/layout/navigation/Navigation.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { IoMdHome, IoMdBook, IoMdSearch } from 'react-icons/io';
+import { IoMdHome, IoMdBook, IoMdSearch, IoMdLogOut } from 'react-icons/io';
 import logo from './logo3.png';
 import { Navbar, Nav } from 'react-bootstrap';
-
 import './Navigation.css';
 
 const Navigation = () => {
     const [scrolling, setScrolling] = useState(false);
     const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 767);
+    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+    const history = useHistory();
 
     const handleScroll = () => {
         if (window.scrollY > window.innerHeight) {
@@ -33,8 +34,16 @@ const Navigation = () => {
         };
     }, []);
 
-    const handleNavItemClick = () => {
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        setIsLoggedIn(false);
+        history.push("/login");
     };
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        setIsLoggedIn(!!token);
+    }, [isLoggedIn]);
 
     return (
         <Navbar variant="light" expand="md" className={`menu ${scrolling ? 'hidden' : ''} ${isMobileView ? 'mobile-view' : ''}`} fixed="top">
@@ -54,30 +63,38 @@ const Navigation = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    { }
-                    <Link to="/" className={`nav-icon d-none d-md-block ${isMobileView ? 'mobile-view' : ''}`} onClick={handleNavItemClick}>
+                    <Link to="/" className={`nav-icon d-none d-md-block ${isMobileView ? 'mobile-view' : ''}`}>
                         <IoMdHome size={24} className="ios-icon" />
                         <span className="nav-text">Home</span>
                     </Link>
-                    <Link to="/courses" className={`nav-icon d-none d-md-block ${isMobileView ? 'mobile-view' : ''}`} onClick={handleNavItemClick}>
+                    <Link to="/courses" className={`nav-icon d-none d-md-block ${isMobileView ? 'mobile-view' : ''}`}>
                         <IoMdBook size={24} className="ios-icon" />
                         <span className="nav-text">Courses</span>
                     </Link>
-                    <Link to="/courses" className={`nav-icon d-none d-md-block ${isMobileView ? 'mobile-view' : ''}`} onClick={handleNavItemClick}>
+                    <Link to="/courses" className={`nav-icon d-none d-md-block ${isMobileView ? 'mobile-view' : ''}`}>
                         <IoMdSearch size={24} className="ios-icon" />
                         <span className="nav-text">Search</span>
                     </Link>
-
-                    { }
-                    <Link to="/" className={`nav-link d-md-none ${isMobileView ? 'mobile-view' : ''}`} onClick={handleNavItemClick}>
+                    {isLoggedIn && (
+                        <div className={`nav-icon d-none d-md-block ${isMobileView ? 'mobile-view' : ''}`} onClick={handleLogout}>
+                            <IoMdLogOut size={24} className="ios-icon" />
+                            <span className="nav-text">Logout</span>
+                        </div>
+                    )}
+                    <Link to="/" className={`nav-link d-md-none ${isMobileView ? 'mobile-view' : ''}`}>
                         Home
                     </Link>
-                    <Link to="/courses" className={`nav-link d-md-none ${isMobileView ? 'mobile-view' : ''}`} onClick={handleNavItemClick}>
+                    <Link to="/courses" className={`nav-link d-md-none ${isMobileView ? 'mobile-view' : ''}`}>
                         Courses
                     </Link>
-                    <Link to="/courses" className={`nav-link d-md-none ${isMobileView ? 'mobile-view' : ''}`} onClick={handleNavItemClick}>
+                    <Link to="/courses" className={`nav-link d-md-none ${isMobileView ? 'mobile-view' : ''}`}>
                         Search
                     </Link>
+                    {isLoggedIn && (
+                        <div className={`nav-link d-md-none ${isMobileView ? 'mobile-view' : ''}`} onClick={handleLogout}>
+                            Logout
+                        </div>
+                    )}
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
