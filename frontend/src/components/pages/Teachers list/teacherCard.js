2fc536e486dce0cb8fb23f2dc15ea184c3a021ca
ahnaf217 <ahnaflabib56@gmail.com>
2024-07-07 11:14:27 +0600

    Added teacher list

---
+++ frontend/src/components/pages/Teachers list/teacherCard.js
@@ -0,0 +1,43 @@
+import { Link } from 'react-router-dom'
+import { Col, Card, Image, Row } from 'react-bootstrap'
+import { motion } from 'framer-motion'
+import './Teacher-card.css'
+import FavButton from '../../shared/FavButton/FavButton'
+
+const TeacherCard = props => {
+
+    const isTeacherFavBtn = true
+
+    return (
+        <Col xs={{ span: 10, offset: 1 }} sm={{ span: 6, offset: 0 }} md={4} lg={3}>
+            <Card className="teacher-card">
+                <motion.div
+                    key='teacher-card'
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 1 }}
+                    transition={{ type: 'spring', stiffness: 150, duration: 1.2 }}
+                >
+                    <Link className="teacher-card-link d-flex justify-content-center" to={`/teachers/${props._id}`}>
+                        <Image className="teacher-card-img mt-4" roundedCircle src={props.imageUrl} alt={`${props.name} ${props.surname}`} />
+                    </Link>
+                </motion.div>
+
+                <Card.Body>
+                    <Card.Title className="teacher-title mb-2">{props.name} {props.surname}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted details">{props.jobOccupation.substring(0, 25)}...</Card.Subtitle>
+
+                    <Card.Text></Card.Text>
+                    <Row>
+                        <Col className="d-flex align-items-center justify-content-between">
+                            <Link className="btn btn-outline-secondary teacher-view-more" to={`/teachers/${props._id}`}>View details</Link>
+                            {props.userInfo &&
+                                <FavButton isTeacherFavBtn={isTeacherFavBtn} updateFavTeachers={props.updateFavTeachers} userInfo={props.userInfo} teacher={props.teacherInfo} itemInfo={props} />}
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+}
+
+export default TeacherCard
---
+++ frontend/src/components/pages/Teachers list/teachersList.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import { motion } from 'framer-motion'
+import TeachersService from '../../../service/teachers.service'
+import SearchBar from './../../shared/SearchBar/SearchBar'
+import NoMatchesMsg from './../../shared/NoMatches-message/NoMatches-msg'
+import TeacherCard from './teacherCard'
+import Loader from '../../shared/Spinner/Loader'
+import './TeacherList.css'
+import { Container, Row } from 'react-bootstrap'
+
+class TeachersList extends Component {
+    constructor() {
+        super()
+        this.state = {
+            teachers: undefined,
+            filteredTeachers: []
+        }
+        this.teachersService = new TeachersService()
+    }
+
+    componentDidMount = () => this.refreshTeachers()
+
+    refreshTeachers = () => {
+        this.teachersService
+            .getTeachers()
+            .then(res => this.setState({ teachers: res.data, filteredTeachers: [...res.data] }))
+            .catch(() => {
+                this.props.history.push('/')
+                this.props.handleToast(true, 'An error has occurred, please try again later', '#f8d7da')
+            })
+    }
+
+    filterBySearch = value => this.setState({ filteredTeachers: [...this.state.teachers].filter(elm => elm.name.toLowerCase().includes(value.toLowerCase())) })
+
+    sortBy = option => {
+        const filteredTeachersCopy = [...this.state.filteredTeachers]
+        switch (option) {
+            case 'Name-A': this.setState({ filteredTeachers: filteredTeachersCopy.sort((a, b) => (a.name > b.name) ? 1 : -1) })
+                break;
+            case 'Name-Z': this.setState({ filteredTeachers: filteredTeachersCopy.sort((a, b) => (a.name < b.name) ? 1 : -1) })
+                break;
+            default: this.setState({ filteredTeachers: [...this.state.teachers] })
+                break;
+        }
+    }
+
+    render() {
+        return (
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+
+                <section className="container-fluid teacher-hero">
+                    <div className="heading">
+                        <Container>
+                            <h1 className="mt-5">Our teachers</h1>
+                            <p>Help people learn new skills, advance their careers, <br></br> and explore their hobbies by sharing your knowledge.</p>
+                            <SearchBar filterBySearch={this.filterBySearch} sortBy={this.sortBy} />
+                        </Container>
+                    </div>
+                </section>
+                <section className="teachers-list">
+                    <Container>
+                        <Row>
+                            {this.state.teachers && this.state.filteredTeachers ?
+                                this.state.filteredTeachers.length === 0 ?
+                                    <NoMatchesMsg />
+                                    :
+                                    this.state.filteredTeachers.map(elm =>
+                                    <TeacherCard key={elm._id} {...elm} userInfo={this.props.loggedUser} teacher={this.props.teacherInfo} updateFavTeachers={this.props.updateFavTeachers} />)
+                                :
+                                <Loader />
+                            }
+                            
+                            {/* {this.state.teachers ? this.state.filteredTeachers.map(elm =>
+                                <TeacherCard key={elm._id} {...elm} userInfo={this.props.loggedUser} teacher={this.props.teacherInfo} updateFavTeachers={this.props.updateFavTeachers} />)
+                                :
+                                <Loader />
+                            } */}
+                        </Row>
+                    </Container>
+                </section>
+            </motion.div>
+        )
+    }
+}
+
+export default TeachersList
